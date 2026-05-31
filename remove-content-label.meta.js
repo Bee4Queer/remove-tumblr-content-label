794c2a8898002d3eebfbf6e9356bd03bbc20841a
Bee4Queer <averse.abfun@gmail.com>
2026-05-30 19:26:06 -0500

    Update remove-content-label.meta.js

--- remove-content-label.meta.js
+++ remove-content-label.meta.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Remove Content Label
 // @namespace    https://verygay.fyi
-// @version      2026-03-09
+// @version      2026-05-30
 // @description  removes tumblr's content label cover things
 // @author       @queeeeeeeeeeeeeeeeeeeeeeeeeeer
 // @include      https://tumblr.com/*
 // @include      https://*.tumblr.com/*
-// @icon         https://raw.githubusercontent.com/VivIsBee/remove-tumblr-content-label/refs/heads/main/icon.png
+// @icon         https://raw.githubusercontent.com/Bee4Queer/remove-tumblr-content-label/refs/heads/main/icon.png
 // @grant        none
-// @updateURL    https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.meta.js
-// @downloadURL  https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.user.js
-// @supportURL   https://github.com/VivIsBee/remove-tumblr-content-label
-// @homepage     https://github.com/VivIsBee/remove-tumblr-content-label
+// @updateURL    https://github.com/Bee4Queer/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.meta.js
+// @downloadURL  https://github.com/Bee4Queer/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.user.js
+// @supportURL   https://github.com/Bee4Queer/remove-tumblr-content-label
+// @homepage     https://github.com/Bee4Queer/remove-tumblr-content-label
 // @sandbox      DOM
 // @tag          social media
 // @tag          utilities
